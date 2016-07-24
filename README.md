f152620091a0c687d7f04549cbbbdcdeb9225eda
Eric Choi <eric.choi@sendgrid.com>
2016-07-24 01:19:20 -0700

    benchmark added; update readme

--- README.md
+++ README.md
@@ -8,6 +8,18 @@ go get github.com/ericychoi/bloomcache
 source $GOPATH/src/github.com/ericychoi/bloomcache/development.env && bcserver
 ```
 
+### Test
+```bash
+% go test -v -race
+=== RUN   TestServer
+2016/07/23 23:23:03 received Add(): testKey
+2016/07/23 23:23:03 received check(): testKey
+2016/07/23 23:23:03 received check(): differentKey
+--- PASS: TestServer (0.00s)
+PASS
+ok  	github.com/ericychoi/bloomcache	8.101s
+```
+
 A test client is provided in `bin/client`
 
 ```bash
@@ -15,6 +27,20 @@ A test client is provided in `bin/client`
 % go run bin/client.go -key test -add
 % go run bin/client.go -key test -check
 2016/07/23 11:49:25 test exists
+```
+
+### Benchmark
+```bash
+% go test -bench=.
+bloomcache2016/07/24 00:59:21 received Add(): testKey
+bloomcache2016/07/24 00:59:21 received check(): testKey
+bloomcache2016/07/24 00:59:21 received check(): differentKey
+PASS
+BenchmarkAddByK3-8   	 2000000	       983 ns/op
+BenchmarkAddByK30-8  	 1000000	      1318 ns/op
+BenchmarkAddByK300-8 	  300000	      5416 ns/op
+BenchmarkAddByK3000-8	   30000	     45674 ns/op
+ok  	github.com/ericychoi/bloomcache	23.114s
 ```
 
 ### How to Build Protobuf
--- bloomcache.go
+++ bloomcache.go
@@ -2,26 +2,37 @@ package bloomcache
 
 import (
 	"log"
+	"os"
 
 	"github.com/ericychoi/bloomcache/protobuf"
 	"github.com/willf/bloom"
 	"golang.org/x/net/context"
 )
 
-// Bloomcache Server
+var Logger *log.Logger = log.New(os.Stdout, `[bloomcache] `, log.LstdFlags)
+
+// Server contains a bloomfilter and handles adding and checking
 type Server struct {
-	BF *bloom.BloomFilter
+	bf *bloom.BloomFilter
 }
 
 //TODO: need AddBatch and CheckBatch (InList call takes a list)
+
+// Add adds a key to the bloomfilter
 func (s *Server) Add(ctx context.Context, in *protobuf.Request) (*protobuf.Response, error) {
-	log.Printf("received Add(): %s", in.Key)
-	s.BF.AddString(in.Key)
+	Logger.Printf("received Add(): %s", in.Key)
+	s.bf.AddString(in.Key)
 	return &protobuf.Response{Error: ""}, nil
 }
 
+// Check checks whether a given key is in the bloom filter or not
 func (s *Server) Check(ctx context.Context, in *protobuf.Request) (*protobuf.CheckResponse, error) {
-	log.Printf("received check(): %s", in.Key)
-	b := s.BF.TestString(in.Key)
+	Logger.Printf("received check(): %s", in.Key)
+	b := s.bf.TestString(in.Key)
 	return &protobuf.CheckResponse{Exists: b}, nil
+}
+
+// New returns a new Server instance given m and k for the bloom filter.
+func New(m int, k int) *Server {
+	return &Server{bf: bloom.New(uint(m), uint(k))}
 }
